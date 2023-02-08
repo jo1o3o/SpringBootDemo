5b913dc987feda027170a4fc24fd3b87ffb08e0b
Jin <joh103089@gmail.com>
2023-02-07 22:35:11 -0800

    Create student service. Add application properties.

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+
+// This is the student service layer. Use the service layer for any business logic.
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+
+}
