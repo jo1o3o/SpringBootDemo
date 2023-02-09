a24bdaf24ba2a0884825a9454214d2e561668953
Jin <joh103089@gmail.com>
2023-02-08 23:57:55 -0800

    Add delete and put methods.

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 // This is the student data layer. Notice that the parent class, JpaRepository
-// contains all the methods that accesses the database. Just give this class
-// the @Repository annotation and specify the entity/object type and id type
-// and Spring Boot does the rest!
+// already contains the basic CRUD methods for student table. Just give this class
+// the @Repository annotation and specify the entity/object type and id type and
+// Spring Boot does the rest.
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    // @Query annotation is not necessary here, but we can include it to be more
+    // specific.
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
-    // This method is equivalent to the following raw SQL query:
-    // SELECT * FROM student WHERE email = ?
     Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,12 +2,14 @@ package com.example.demo.student;
 
 // This is the student service layer. Use the service layer for any business logic.
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -31,5 +33,33 @@ public class StudentService {
             throw new IllegalStateException("Email taken.");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean studentExists = studentRepository.existsById(studentId);
+        if (!studentExists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist.");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    // To get/set entity attributes, method needs to be inside a "transaction".
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist."));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            // If email is already taken, throw exception.
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email " + email + " is already taken.");
+            }
+            student.setEmail(email);
+        }
     }
 }
