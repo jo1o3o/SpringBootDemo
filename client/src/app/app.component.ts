24f8a976c888b4f209049a68ce5a48477c8e067e
Jin <joh103089@gmail.com>
2023-02-24 23:57:39 -0800

    Dark mode toggle changes text between light mode and dark mode.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DarkModeService } from 'angular-dark-mode';
 import { Observable } from 'rxjs/internal/Observable';
 
@@ -8,13 +8,20 @@ import { Observable } from 'rxjs/internal/Observable';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = "College Admissions";
   opened = false;
   darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
+  mode: string;
 
   constructor(private darkModeService: DarkModeService) { }
+  
+  ngOnInit(): void {
+    this.darkMode$.subscribe(darkMode => {
+      darkMode ? this.mode = "Dark Mode" : this.mode = "Light Mode"; 
+    });
+  }
 
   onToggle(): void {
     this.darkModeService.toggle();
